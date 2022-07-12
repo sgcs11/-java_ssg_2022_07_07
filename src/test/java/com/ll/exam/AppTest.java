c92c847529afce800cd052005b1acb2c96dab502
sgcs11 <joachy@naver.com>
2022-07-12 13:26:35 +0900

    Rq에 Map 도입

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,8 +2,7 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
 
     @Test
-    public void 테스트_스캐너() {
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                    등록
                    명언1
@@ -28,6 +27,52 @@ class AppTest {
         assertEquals("등록",cmd);
         assertEquals("명언1",content);
         assertEquals("작가1",author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
+    }
+
+    @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+
+    @Test
+    public void Rq__getPath() {
+        Rq rq = new Rq("삭제?id=1");
+
+        String path = rq.getPath();
+
+        assertEquals("삭제", path);
     }
 
 }
