5a0733100497ac318bc77d8b5928e796c067d749
sgcs11 <joachy@naver.com>
2022-07-12 16:04:32 +0900

    파일생성기능 테스트

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -5,45 +5,22 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class AppTest {
-
+public class AppTest {
     @Test
-    public void 문자열을_스캐너의_입력으로_설정() {
-        String input = """
-                   등록
-                   명언1
-                   작가1
-                """.stripIndent();//문자열 안의 모든 라인에 strip를 적용하여 앞 뒤 공백을 제거함.
-
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        Scanner sc = new Scanner(in);
-
-        String cmd = sc.nextLine().trim();
-        String content = sc.nextLine().trim();
-        String author = sc.nextLine().trim();
-
-        assertEquals("등록",cmd);
-        assertEquals("명언1",content);
-        assertEquals("작가1",author);
+    void 파일에_내용쓰기() {
+        Util.mkdir("test_data");
+        Util.saveToFile("test_data/1.json", "내용");
     }
 
     @Test
-    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
-        // 표준출력을 리다이렉션
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
+    public void Rq__getPath() {
+        Rq rq = new Rq("삭제?id=1");
 
-        System.out.println("안녕");
+        String path = rq.getPath();
 
-        String rs = output.toString().trim();
-
-        // 표준출력을 원상복구
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-        output.close();
-
-        assertEquals("안녕", rs);
+        assertEquals("삭제", path);
     }
 
     @Test
@@ -67,12 +44,45 @@ class AppTest {
     }
 
     @Test
-    public void Rq__getPath() {
-        Rq rq = new Rq("삭제?id=1");
-
-        String path = rq.getPath();
-
-        assertEquals("삭제", path);
+    public void 테스트_실험() {
+        int rs = 10 + 20;
+        assertEquals(30, rs);
     }
 
+    @Test
+    public void 문자열을_스캐너의_입력으로_설정() {
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        // 그 동안 System.out.println 으로 모아놨던 문장들을 받아옴
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
+    }
 }
